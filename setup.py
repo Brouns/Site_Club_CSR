ad09acc4eb905c949f4e9064a0d2325f2b46a6cb
Bruno S <brunoschmit@yahoo.fr>
2022-09-27 21:09:36 +0200

    modifications heroku

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='site_bootstrap',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='bruno',
+    author_email='',
+    description=''
+)
