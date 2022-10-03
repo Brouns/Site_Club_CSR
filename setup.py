31c70b4f829726d5571df14f1c47707554f911cd
Bruno S <brunoschmit@yahoo.fr>
2022-10-03 13:03:21 +0200

    try heroku

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import setup
 setup(
     name='site_bootstrap',
     version='1',
-    packages=[''],
-    url='',
-    license='',
+    packages=['site_bootstrap'],
     author='bruno',
-    author_email='',
-    description=''
+    author_email='goofyywk@yahoo.fr',
+    description='site CSR'
 )
